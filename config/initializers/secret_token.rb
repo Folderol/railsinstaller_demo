30215c832af608ced48eaab39463078a7d3de998
Folderol <huber.robert542@gmail.com>
2012-09-11 15:11:49 -0700

    initial add
    Add asll

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinstallerDemo::Application.config.secret_token = 'cde51a5f770ac3b31d307cd1bc85d400d73c5c0a82ad886f92e23395d5231f4a498dfd34f2c2e49dda98b7dc61a302e39c0ff5b8e96cdde7a14db6ae73424fa8'
